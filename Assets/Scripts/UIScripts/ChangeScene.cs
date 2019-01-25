1dfe22de9a126a5bbb8f42be30783a56d39d2a72
unknown <a.wlochowitz@googlemail.com>
2019-01-25 22:02:21 +0100

    Scene Manager

---
+++ Assets/Scripts/UIScripts/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void ChangeToScene(string sceneToChangeTo)
+    {
+        Application.LoadLevel(sceneToChangeTo);
+    }
+}
