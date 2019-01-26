538e8a3a678ff91be72733cfb5c648a5e8ad863d
Marcel Bulla <gmx@marcel-bulla.de>
2019-01-26 11:12:57 +0100

    Empty Tile Prefab

---
+++ Assets/Scripts/TileLogic/EmptyDropTile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyDropTile : MonoBehaviour
+{
+    bool isDragging = true;
+    private Collider2D _lastCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isDragging && Input.GetMouseButton(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = mousePos;
+        }
+        else if(isDragging && !Input.GetMouseButton(0))
+        {
+            if(_lastCollider != null)
+            {
+                this.transform.position = _lastCollider.transform.position;
+                Destroy(_lastCollider.gameObject);
+                isDragging = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(isDragging)
+        {
+            //Input.GetMouseButton(0)
+            
+            if(other.CompareTag("Tile"))
+            {
+                _lastCollider = other;
+                
+            }
+        }
+    }
+}
