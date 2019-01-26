dd4eedaf1133146aa473f3658994988e395ef28c
unknown <a.wlochowitz@googlemail.com>
2019-01-26 19:58:27 +0100

    Fix Menu Issues and adjust Styles

---
+++ Assets/Scripts/UIScripts/TextHover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TextHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Text theText;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        theText.color = Color.white; //Or however you do your color
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        theText.color = Color.grey; //Or however you do your color
+    }
+}
