cfb865997df1f1768d1e7914de43c9d3eaac189c
unknown <a.wlochowitz@googlemail.com>
2019-01-26 22:49:29 +0100

    add win screen

---
+++ Assets/Scripts/UIScripts/Win.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void LoadNextLevel(int lastLevel)
+    {
+        SceneManager.LoadScene(lastLevel+1);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Splashscreen");
+    }
+}
