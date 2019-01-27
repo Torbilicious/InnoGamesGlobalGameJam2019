d90a18e482a4c65ac4550660a5e2e00360b03b8b
Max Bebök <beboek.max@gmail.com>
2019-01-27 03:16:39 +0100

    new: added NPC for death animations

---
+++ Assets/Scripts/NpcLizard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcLizard : MonoBehaviour
+{
+    uint moveCounter = 0;
+    float randRotScale = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        System.Random rnd = new System.Random();
+        randRotScale = (float)rnd.NextDouble() + 0.8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameState.isDead) 
+        {
+            float rot = (float)Mathf.Sin((float)moveCounter * 0.2f) * randRotScale;
+            transform.Rotate(0.0f, 0.0f, rot);
+            ++moveCounter;
+        }
+    }
+}
