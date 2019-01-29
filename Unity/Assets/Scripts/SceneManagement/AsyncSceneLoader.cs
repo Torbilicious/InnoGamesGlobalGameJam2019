1f6f23464d38c8d7d5a9ab1dd493a99c9263faf0
Marcel Bulla <gmx@marcel-bulla.de>
2019-01-29 13:11:24 +0100

    Added async scene loading, no more lags on slow smartphones like mine :x

---
+++ Unity/Assets/Scripts/SceneManagement/AsyncSceneLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AsyncSceneLoader : MonoBehaviour
+{
+    public static Scene Scene = Scene.MENU_MAIN;
+
+    public GameObject Loader;
+
+    void Start()
+    {
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    private IEnumerator LoadSceneAsync()
+    {
+        string sceneKey = "";
+
+        switch (Scene)
+        {
+            case Scene.MENU_MAIN:
+                sceneKey = "MainMenu";
+                break;
+            case Scene.MENU_CREDITS:
+                sceneKey = "CreditMenu";
+                break;
+            case Scene.MENU_WIN:
+                sceneKey = "WinMenu";
+                break;
+        }
+
+        AsyncOperation op = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneKey);
+        float progress = 0;
+
+        while (!op.isDone)
+        {
+            float add = op.progress - progress;
+            this.Loader.transform.Rotate(0, 0, add * 360.0f);
+            progress = op.progress;
+            yield return null;
+        }
+    }
+}
--- Unity/Assets/Scripts/SceneManagement/SceneManagerExtensions.cs
+++ Unity/Assets/Scripts/SceneManagement/SceneManagerExtensions.cs
@@ -1,23 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public static class SceneManager
 {
     public static void LoadScene(Scene scene)
     {
-        switch(scene)
-        {
-            case Scene.MENU_MAIN:
-                UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
-                break;
-            case Scene.MENU_CREDITS:
-                UnityEngine.SceneManagement.SceneManager.LoadScene("CreditMenu");
-                break;
-            case Scene.MENU_WIN:
-                UnityEngine.SceneManagement.SceneManager.LoadScene("WinMenu");
-                break;
-        }
+        AsyncSceneLoader.Scene = scene;
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Loader");
     }
 }
